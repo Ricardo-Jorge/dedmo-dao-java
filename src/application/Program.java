64bd0d23fb98b319f1f2c69087f33b32ff1a134a
Ricardo Jorge <ricardolemos18@hotmail.com>
2025-05-13 16:13:34 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,14 @@ public class Program {
 		Seller newSeller =  new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(9);
+		seller.setName("Greg Doe");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
+		
+		
 
 }
--- src/moodel/dao/impl/SellerDaoJDBC.java
+++ src/moodel/dao/impl/SellerDaoJDBC.java
@@ -26,7 +26,7 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		PreparedStatement st = null;		
+		PreparedStatement st = null;
 
 		try {
 			st = conn.prepareStatement("INSERT INTO seller\r\n"
@@ -53,20 +53,39 @@ public class SellerDaoJDBC implements SellerDao {
 				throw new DbException("Unexpected error! No rows affected!");
 			}
 
-		} 
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		} 
-		finally {
+		} finally {
 			DB.closeStatement(st);
-			
+
 		}
 
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE seller\r\n"
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?\r\n"
+					+ "WHERE Id = ?");
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		
 
 	}
 
